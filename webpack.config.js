5948ce0b82b3f8db6872b0595a9e77a78298acf6
JellyLu <634725546@qq.com>
2019-05-13 15:16:12 +0800

    [Yangjie] - Add html-webpack-plugin to generate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: './app.bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React Awesome"
+    })
+  ]
 };
