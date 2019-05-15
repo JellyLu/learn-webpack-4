ead111c41f12b9382c99fbfc25c72264fe0f0b74
JellyLu <634725546@qq.com>
2019-05-15 17:30:10 +0800

    [Yangjie] - use babel to convert react jsx to es5.

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 
-class Root extends React.Component {
-  render() {
-    return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello Webpack</h1>
-      </div>
-    );
-  }
-}
+const Root = () => (<div style={{textAlign: 'center'}}><h1>Hello Webpack</h1></div>);
 
 export default Root;
