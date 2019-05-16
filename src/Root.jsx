5311e821067dc4bdb5b79352138fab265eb965eb
JellyLu <634725546@qq.com>
2019-05-16 11:33:07 +0800

    [Yangjie] - Use `file-loader` and `html-loader` to pack images

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Root = () => (<div style={{textAlign: 'center'}}>
+  <div className='hotpot-train'/>
   <h1>Hello Webpack</h1>
   <h1 style={{color: 'black'}}>Hello Webpack</h1>
+  <div className='hotpot'/>
 </div>);
 
 export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,24 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-    }]
+    }, {
+      test: /\.gif$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'images/'
+      }
+    },
+      {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true
+        }
+      }],
+    }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
